7648379e5f0cfdf6f65c9005dda82971eea0ac9a
Christopher <gusandchbh@student.gu.se>
2023-02-28 18:33:35 +0100

    lol

---
+++ concatenateStrings/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    char* str1 = "I love"; // string1
+    char* str2 = " DIT632"; // string2
+
+    int len1 = strlen(str1); // get length of string1
+    int len2 = strlen(str2); // get length of string2
+
+    char* combinedStr = (char*) malloc(sizeof(char) * (len1 + len2 + 1)); // allocate memory for combined string
+
+    // copy string1 to combined string
+    for (int i = 0; i < len1; i++) {
+        combinedStr[i] = str1[i];
+    }
+
+    // copy string2 to combined string
+    for (int i = 0; i < len2; i++) {
+        combinedStr[len1 + i] = str2[i];
+    }
+
+    combinedStr[len1 + len2] = '\0'; // add null terminator to end of combined string
+
+    printf("Concatenated string: %s", combinedStr); // print combined string
+
+    free(combinedStr); // free memory allocated for combined string
+
+    return 0;
+}
