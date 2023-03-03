342a0600c066760919ea6f0a8f9b38b0e112b6fd
Christopher <gusandchbh@student.gu.se>
2023-03-03 16:00:45 +0100

    lol

---
+++ 2. Print unsigned 32bit integer/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void print_bin(uint32_t n) {
+    for (int i = 31; i >= 0; i--) {
+        printf("%d", (n >> i) & 1);
+        if (i % 8 == 0 && i != 0) {
+            printf(".");
+        }
+    }
+}
+
+int main() {
+    uint32_t num1 = 0;
+    uint32_t num2 = 1;
+    uint32_t num3 = 2;
+    uint32_t num4 = 43;
+    uint32_t num5 = 123456789;
+
+    printf("num1: ");
+    print_bin(num1);
+    printf("\n");
+
+    printf("num2: ");
+    print_bin(num2);
+    printf("\n");
+
+    printf("num3: ");
+    print_bin(num3);
+    printf("\n");
+
+    printf("num4: ");
+    print_bin(num4);
+    printf("\n");
+
+    printf("num5: ");
+    print_bin(num5);
+    printf("\n");
+
+    return 0;
+}
