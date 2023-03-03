5d3ef26ffa9eb3e540ed984476c19d3a40f546fa
Christopher <gusandchbh@student.gu.se>
2023-03-03 16:16:27 +0100

    lol

--- 2. Print unsigned 32bit integer/main.c
+++ 2. Print unsigned 32bit integer/main.c
@@ -9,6 +9,7 @@ void print_bin(uint32_t n) {
         }
     }
 }
+uint16_t make_16bit(uint8_t least_significant, uint8_t most_significant);
 
 int main() {
     uint32_t num1 = 0;
@@ -35,7 +36,29 @@ int main() {
 
     printf("num5: ");
     print_bin(num5);
+    printf("\n\n");
+
+    printf("PART TWO\n\n");
+
+    uint8_t least_significant = 7;
+    printf("7: ");
+    print_bin(least_significant);
     printf("\n");
 
+    uint8_t most_significant = 1;
+    printf("1: ");
+    print_bin(most_significant);
+    printf("\n");
+
+    printf("Combined:");
+    print_bin(make_16bit(7, 1));
+    printf("\n");
+
+
     return 0;
+}
+
+uint16_t make_16bit(uint8_t least_significant, uint8_t most_significant){
+        uint16_t result = most_significant << 8 | least_significant;
+        return result;
 }
