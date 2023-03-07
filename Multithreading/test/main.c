b19066170187ac4bf5af66bb452905f146dc1847
Christopher <gusandchbh@student.gu.se>
2023-03-07 12:42:50 +0100

    lol

--- Multithreading/test/main.c
+++ Multithreading/test/main.c
@@ -2,6 +2,9 @@
 #include <unistd.h>
 #include <pthread.h>
 
+// This function returns a void pointer and takes a void pointer as an argument
+// The reason it does this is because it's generic, a void pointer can point to anything
+// This means we can return any datatype and pass any datatype to the thread function
 void* thread_func(void *arg){
     for (int i = 0; i < 8; i++)
     {
